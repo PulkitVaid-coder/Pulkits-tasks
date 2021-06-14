6b1d5b59f0752f4e173ba9006322b10aed6391f2
PulkitVaid-coder <pulkit.vaid@punchh.com>
2021-06-14 14:16:25 +0530

    Flatlist Task

--- App.js
+++ App.js
@@ -1,112 +1,104 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next: 
-          </Section>
-          <LearnMoreLinks />
+function Item({ item }) {
+  if (item.index % 2 == 0) {
+    return (
+      
+      <View style={styles.listItem1}>
+        <Image source={{ uri: item.photo }} style={{ width: 60, height: 60, borderRadius: 30,}} />
+        <View style={{ alignItems: "flex-end", flex: 1,margin:15 }}>
+          <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+          <Text>{item.message}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      </View>
+    );
+  }
+  return (
+    
+     <View style={styles.listItem}>
+        <Image source={{ uri: item.photo }} style={{ width: 60, height: 60, borderRadius: 30 }} />
+        <View style={{ alignItems: "flex-start", flex: 1,margin :15 }}>
+          <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+          <Text>{item.message}</Text>
+        </View>
+      </View>
+  )
+}
+
+export default class App extends React.Component {
+  state = {
+    data:[
+        {
+            "name": "Michael Jordan",
+            "message": " Hello there",
+            "photo": "https:\/\/randomuser.me\/api\/portraits\/men\/43.jpg",
+            "index": 1
+            
+        },
+        {
+            "name": "Alexa",
+            "message": "Hey",
+            "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/44.jpg",
+            "index":2
+            
+        },
+        {
+            "name": "Michelle",
+            "message": "How are you guys ?",
+            "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/68.jpg",
+            "index":3
+        },
+        {
+            "name": "Ann",
+            "message": "we are good",
+            "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/65.jpg",
+            "index":4
+        },
+
+    ]
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={{flex:1}}
+          data={this.state.data}
+          renderItem={({ item }) => <Item item={item}/>}
+          keyExtractor={item => item.index}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7',
+    marginTop:60
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  listItem:{
+    margin:10,
+    padding:10,
+    backgroundColor:"rgb(144,238,144)",
+    width:"80%",
+    flex:1,
+    alignSelf:"center",
+    flexDirection:"row",
+    borderRadius:5
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  listItem1:{
+    margin:10,
+    padding:10,
+    backgroundColor:"rgb(173,216,230)",
+    width:"80%",
+    flex:1,
+    alignSelf:"center",
+    flexDirection:"row-reverse",
+    borderRadius:5
+  }
 });
-
-export default App;
